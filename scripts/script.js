366516e0653b5eb7a534233d7d53c97a8989f8fa
Lauren Hetherington <laurenlhetherington@gmail.com>
2020-01-08 12:41:34 -0500

    Added hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
+    // items within the portfolio section to reveal insides on mobile / on click
     $('.itemContainer').on('click',function() {
         $('.itemContainer').removeClass('selected');
         $(this).addClass('selected');
     });
+
+    // open the hamburger menu!
+    $('.hamburgerButton').on('click',function() {
+        $(this).toggleClass('open');
+        $('.menu').toggleClass('open');
+    });
+
 });
